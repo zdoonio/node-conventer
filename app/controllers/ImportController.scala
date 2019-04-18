15824202ab517cbb4fa8599230fb51759e1a8a3a
zdoonio <dominik.zdunczyk@gmail.com>
2019-04-18 16:18:39 +0200

    refactor: moved parsing logic to models layer

--- app/controllers/ImportController.scala
+++ app/controllers/ImportController.scala
@@ -1,9 +1,8 @@
 package controllers
 
-import model.{Node, NodeData, PersistanceContext}
+import model.Node
 import play.api.mvc.{Action, Controller}
-import org.apache.poi.ss.usermodel.{Workbook, WorkbookFactory}
-import PersistanceContext._
+
 
 /**
   * Created by Dominik Zduńczyk on 17.04.19.
@@ -12,34 +11,12 @@ object ImportController extends Controller {
 
   def importNodes = Action(parse.multipartFormData) { implicit request =>
     request.body.file("sheet").map { content =>
-
-      def load: Workbook =
-        WorkbookFactory.create(content.ref.file)
-
-      val sheet = load.getSheetAt(0)
-      val cells = for (r <- 1 to sheet.getLastRowNum; c <- 0 to 3)
-         yield sheet.getRow(r).getCell(c)
-
-      val cellsToMap = cells.filterNot(_.toString == "").grouped(2).map { row =>
-        (row(0), row(1))
-      }.toList
-
-      val nodeData = cellsToMap.map { row =>
-          NodeData(row._2.getNumericCellValue.toInt, row._1.toString, List())
+      try {
+        Node.parseNodes(content.ref.file)
+      } catch {
+        case e: Exception =>
+          InternalServerError
       }
-
-      val groupNodes = nodeData.partition(_.name.length == 1)
-
-      val nodesAndSubNodes = groupNodes._1.map { mainNodeData =>
-        (mainNodeData, groupNodes._2.filter(mainNodeData.name.charAt(0) == _.name.charAt(0)))
-      }
-
-      nodesAndSubNodes.foreach { nodeAndSubNode =>
-        transactional {
-          Node.create(nodeAndSubNode._1, nodeAndSubNode._2)
-        }
-      }
-
       Ok
     }.getOrElse {
       BadRequest
--- app/model/Node.scala
+++ app/model/Node.scala
@@ -1,7 +1,11 @@
 package model
 
+import java.io.File
+
 import net.fwbrasil.activate.entity.{Entity, LazyList}
+import org.apache.poi.ss.usermodel.{Workbook, WorkbookFactory}
 import PersistanceContext._
+
 
 /**
   * Created by Dominik Zduńczyk on 17.04.19.
@@ -23,6 +27,35 @@ object Node {
       Node(subNodeData.nodeId, subNodeData.name, Some(node))
     }
 
+  }
+
+  def parseNodes(file: File) = {
+    def load: Workbook =
+      WorkbookFactory.create(file)
+
+    val sheet = load.getSheetAt(0)
+    val cells = for (r <- 1 to sheet.getLastRowNum; c <- 0 to 3)
+      yield sheet.getRow(r).getCell(c)
+
+    val cellsToMap = cells.filterNot(_.toString == "").grouped(2).map { row =>
+      (row(0), row(1))
+    }.toList
+
+    val nodeData = cellsToMap.map { row =>
+      NodeData(row._2.getNumericCellValue.toInt, row._1.toString, List())
+    }
+
+    val groupNodes = nodeData.partition(_.name.length == 1)
+
+    val nodesAndSubNodes = groupNodes._1.map { mainNodeData =>
+      (mainNodeData, groupNodes._2.filter(mainNodeData.name.charAt(0) == _.name.charAt(0)))
+    }
+
+    nodesAndSubNodes.foreach { nodeAndSubNode =>
+      transactional {
+        Node.create(nodeAndSubNode._1, nodeAndSubNode._2)
+      }
+    }
   }
 
   def getMainNodeList: List[Node] = transactional(select[Node] where(_.mainNode.isNull))
